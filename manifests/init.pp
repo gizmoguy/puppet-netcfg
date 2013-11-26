95ef0ad22d7cb3b88b38c6920d5a72f64daebcb8
Brad Cowie <brad@gizmoguy.net.nz>
2013-11-26 13:23:50 +1300

    Add support for network_route resource.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,13 @@
   #
 class netcfg(
   $interfaces = $netcfg::params::interfaces,
+  $routes     = $netcfg::params::routes,
 ) inherits netcfg::params {
 
   validate_hash($interfaces)
   create_resources('network_config', $interfaces)
+
+  validate_hash($routes)
+  create_resources('network_route', $routes)
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,5 @@
 class netcfg::params (
 ){
   $interfaces = {}
+  $routes     = {}
 }
